accf37fec167a0a74b53c2cd6b064b6b8bd75182
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2023-04-18 15:11:16 +0200

    implemented roberta xlm (yet to run and debug)

--- RoBertaClassifier.py
+++ RoBertaClassifier.py
@@ -1,17 +1,17 @@
 from torch import nn
 import torch
-from transformers import BertModel
+from transformers import RobertaModel
 import transformers
 import numpy as np
 
-class BertClassifier(nn.Module) :
+class RoBertaClassifier(nn.Module) :
 
-    def __init__(self, bert_path = 'dbmdz/bert-base-italian-xxl-cased', fine_tune = True, head = None):
+    def __init__(self, roberta_path = 'xlm-roberta-large', fine_tune = True, head = None):
         
-        super(BertClassifier, self).__init__()
+        super(RoBertaClassifier, self).__init__()
 
         #BERT initialization
-        self.bert = BertModel.from_pretrained(bert_path)
+        self.roberta = RobertaModel.from_pretrained(roberta_path)
 
         #Classifier initialization
         if head is not None and head._modules['0'].in_features == 768:
@@ -29,18 +29,18 @@ class BertClassifier(nn.Module) :
         
         #Freezing for fine tuning
         if not fine_tune:
-            for param in self.bert.parameters():
+            for param in self.roberta.parameters():
                 param.requires_grad = False
 
     def forward(self, inputs, mask):
         # Feeding the input to Bert
-        last_hidden_states = self.bert(input_ids=inputs, attention_mask=mask)
+        last_hidden_states = self.roberta(input_ids=inputs, attention_mask=mask)
 
         # Taking only the CLS : [ all sentences (:), only the first position (0), all hidden unit outputs (:) ] 
-        bert_cls = last_hidden_states[0][:,0,:]
+        roberta_cls = last_hidden_states[0][:,0,:]
 
         # Classification layer forward
-        logits_preds = self.head(bert_cls)
+        logits_preds = self.head(roberta_cls)
 
         # The shape of the softmax_preds is #number of sentences * #number of classes
         return logits_preds
@@ -52,15 +52,15 @@ class BertClassifier(nn.Module) :
 
 
 # Returns initialized model
-def init_bert_clf(tr_steps, lr_rate=1e-5, scheduler_warmp_steps=None, head=None):
-    bert_clf = BertClassifier(head=head)
+def init_roberta_clf(tr_steps, lr_rate=1e-5, scheduler_warmp_steps=None, head=None):
+    roberta_clf = RoBertaClassifier(head=head)
 
     if scheduler_warmp_steps == None:
         scheduler_warmp_steps = int(tr_steps/10)
 
     loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = transformers.AdamW(params = bert_clf.parameters(), lr=lr_rate, correct_bias=False)
+    optimizer = transformers.AdamW(params = roberta_clf.parameters(), lr=lr_rate, correct_bias=False)
     scheduler = transformers.get_cosine_schedule_with_warmup(optimizer=optimizer, num_training_steps=tr_steps, num_warmup_steps=scheduler_warmp_steps)
     
     
-    return bert_clf, loss_function, optimizer, scheduler
+    return roberta_clf, loss_function, optimizer, scheduler
